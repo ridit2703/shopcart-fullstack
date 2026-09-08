02db7fa19c2f9bf0c65f2e2888b3d5e344a294c2
Ridit2703 <ridit2327@gmail.com>
2026-09-08 08:53:51 +0530

    fix some bugs in backend

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -1,22 +1,3 @@
-// import mongoose from "mongoose";
-// const addressSchema=new mongoose.Schema({
-//     userId:{
-//         type:mongoose.Schema.Types.ObjectId,
-//         required:true,
-//         ref:'User'
-//     },
-//     fullName:String,
-//     phone:String,
-//     addressLine:String,
-//     city:String,
-//     state:String,
-//     postalCode:String,
-//     country:String,
-//     pincode:String
-// },{
-//     timestamps:true
-// });
-// export default mongoose.model('Address',addressSchema);
 
 import mongoose from 'mongoose';
 
